64e11c1070227cd17f81711ba3eef34116cfefcf
oyeleyesesan <oyeleyesesan@gmail.com>
2019-06-15 14:10:26 +0100

    I change the action method

--- MyFirstAssignment/Controllers/HomeController.cs
+++ MyFirstAssignment/Controllers/HomeController.cs
@@ -34,10 +34,5 @@ namespace MyFirstAssignment.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
